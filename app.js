e5a2a63ded015390e79ac34c0a82230944f2bd71
aznf.lubis <arlyzeina24@gmail.com>
2023-01-21 15:15:55 +0700

    Home API

--- app.js
+++ app.js
@@ -20,8 +20,6 @@ db.then(() => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server Running at Port: ${PORT}`));
 
-// Output Umum
-app.use(console.log("Welcome To API Test-CodingLab"))
 
 // Set Body Parser
 app.use(bodyParser.json());
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const siswaRouter = require("./siswa.router");
 // const guruRouter = require("./guru.router");
 // const kelasRouter = require("./kelas.router");
 
+// Output Umum
+router.use("/", console.log("Welcome To API Test-CodingLab"))
+
 router.use("/siswa", siswaRouter);
 // router.use("/guru", guruRouter);
 // router.use("/kelas", kelasRouter);
