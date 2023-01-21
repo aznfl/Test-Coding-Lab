038f741b42b63cffcfff07133cbc9b9ca92f26ff
aznf.lubis <arlyzeina24@gmail.com>
2023-01-21 14:48:00 +0700

    note guru

--- routes/guru.router.js
+++ routes/guru.router.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const router = express.Router();
-const siswaController = require("../controllers/siswa.controller.js");
+// const express = require("express");
+// const router = express.Router();
+// const siswaController = require("../controllers/siswa.controller.js");
 
-router.get("/", siswaController.getAllSiswa);
+// router.get("/", siswaController.getAllSiswa);
 
-// router.get("/:id", siswaController.getAllsiswaById);
+// // router.get("/:id", siswaController.getAllsiswaById);
 
-router.post("/", siswaController.createSiswa);
+// router.post("/", siswaController.createSiswa);
 
-router.put("/:id", siswaController.updateSiswaById);
+// router.put("/:id", siswaController.updateSiswaById);
 
-router.delete("/:id", siswaController.deleteSiswaById);
+// router.delete("/:id", siswaController.deleteSiswaById);
 
-module.exports = router;
+// module.exports = router;
